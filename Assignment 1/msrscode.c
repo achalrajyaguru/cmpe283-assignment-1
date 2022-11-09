d9795b4aa992bc2895b6da91d55d8275fff53bbd
achalrajyaguru <rajyaguruachal@gmail.com>
2022-11-08 22:22:35 -0800

    pushing the msrscode.c file with libraries in it

---
+++ Assignment 1/msrscode.c
@@ -0,0 +1,31 @@
+/*  
+ *  Name:Achal Rajyaguru || ID: 015958670
+ */
+#include <linux/module.h>	
+#include <linux/kernel.h>	
+#include <asm/msr.h>
+
+#define MAX_MSG 80
+
+
+#define IA32_VMX_PINBASED_CTLS	0x481
+
+#define IA32_VMX_BASIC_CTLS 0x480
+
+#define IA32_VMX_PROCBASED_CTLS 0x482
+
+#define IA32_VMX_PROCBASED_CTLS2 0x48B
+
+#define IA32_VMX_EXIT_CTLS 0x483
+
+#define IA32_VMX_ENTRY_CTLS 0x484
+
+#define IA32_VMX_TRUE_PINBASED_CTLS 0x48D
+
+#define IA32_VMX_TRUE_PROCBASED_CTLS 0x48E
+
+#define IA32_VMX_TRUE_EXIT_CTLS 0x48F
+
+#define IA32_VMX_TRUE_ENTRY_CTLS 0x490
+
+
